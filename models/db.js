2ec795bcd92141c41d4cd1152490747757b92520
叶嘉 <1141151662@qq.com>
2021-02-22 23:25:21 +0800

    11

---
+++ models/db.js
@@ -0,0 +1,15 @@
+const {
+  Sequelize
+} = require("sequelize");
+const {
+  sqlLogger
+} = require("../logger");
+const sequelize = new Sequelize("myschooldb", "root", "kuang321", {
+  host: "localhost",
+  dialect: "mysql",
+  logging: (msg) => {
+    sqlLogger.debug(msg);
+  },
+});
+
+module.exports = sequelize;
